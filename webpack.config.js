167e69520cb8a32a43bdd68a59b1c9d604c9f048
Taejung Kim <hi2177@naver.com>
2022-02-01 00:54:25 +0900

    🎉 init: 웹팩, 바벨 등 개발환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry : './src/index.js',
+  output : {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'development',
+  module : {
+    rules: [{ test: /\.js$/, use: 'babel-loader' },
+    {
+      test: /\.css$/,
+      use: [
+        { loader: MiniCssExtractPlugin.loader },
+        { loader: 'css-loader', options: { import: true }},
+      ],
+    },],
+  },
+  devServer: {
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }), 
+    new MiniCssExtractPlugin()
+  ],
+}
